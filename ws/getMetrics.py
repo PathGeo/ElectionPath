2979b590b162a3d7c846f3a840731349afb17449
Chris Allen <cccchris.allen@gmail.com>
2013-10-31 14:49:57 -0700

    Added top users in time period for getMetrics.py

--- ws/getMetrics.py
+++ ws/getMetrics.py
@@ -28,10 +28,12 @@ try:
 	hashCounter = Counter([hashtag for t in entitySets if 'hashtags' in t['entities'] for hashtag in t['entities']['hashtags']])
 	urlCounter = Counter([url for t in entitySets if 'long_urls' in t['entities'] for url in t['entities']['long_urls']])
 	mentionCounter = Counter([mention for t in entitySets if 'user_mentions' in t['entities'] for mention in t['entities']['user_mentions']])
+	userCounter = Counter([t['user']['screen_name'] for t in tweets])
 
 	topUrls = urlCounter.most_common(MAX_RESULTS)
 	topHashes = hashCounter.most_common(MAX_RESULTS)
 	topMentions = mentionCounter.most_common(MAX_RESULTS)
+	topUsers = userCounter.most_common(MAX_RESULTS)
 	#topMentions = sorted(mentionCounter.items(), key=lambda x: x[1], reverse=True)[:RESULTS_MAX]
 
 
@@ -50,6 +52,7 @@ try:
 	output['urls'] = [{'value': item[0], 'count': item[1], 'rank': indx + 1} for indx, item in enumerate(topUrls)]
 	output['hashtags'] = [{'value': item[0], 'count': item[1], 'rank': indx + 1} for indx, item in enumerate(topHashes)]
 	output['mentions'] = [{'value': item[0], 'count': item[1], 'rank': indx + 1} for indx, item in enumerate(topMentions)]
+	output['users'] = [{'value': item[0], 'count': item[1], 'rank': indx + 1} for indx, item in enumerate(topUsers)]
 	output['retweets'] = topRTTexts
 
 	print ''
