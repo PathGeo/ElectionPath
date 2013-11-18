bd1d2a6f5b47d4baf22bf80f885f76801da5da5c
Calvin Jung <ediyacoo@gmail.com>
2013-11-18 18:54:11 +0300

    Fix countdown clock

--- js/main.js
+++ js/main.js
@@ -178,9 +178,10 @@
 	function init_time(){
 		//calcualte countdown
 		var todayTime=new Date().getTime(),
-			electionTime=new Date(2013,11-1,19, 8,0,0).getTime(),
+			electionTime=new Date(2013,11-1,19, 0,0,0).getTime(),
 			//electionTime=new Date("November 19, 2013 08:00:00"),//.getTime(),
 			countdownTime=parseInt((electionTime-todayTime)/86400/1000)+1;
+			countdownTime=(countdownTime<0)?0:countdownTime;
 		$("#countdown label").html(countdownTime);
 		
 		
