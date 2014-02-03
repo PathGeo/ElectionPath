82f6229b204d11cd77a8a4a7ca9ca0de5d404678
Calvin Jung <ediyacoo@gmail.com>
2014-02-03 02:28:58 -0800

    getMetric.py
    1. accept multiple candidate separate by ","
    2. uniform output object name

--- ws/getMetrics.py
+++ ws/getMetrics.py
@@ -15,52 +15,69 @@ HASH_URL = 'https://twitter.com/search?q=%%23%s&src=hash'
 try: 
 	fields = cgi.FieldStorage()
 
-	candidateName = fields['candidate'].value.title()
+	candidateName = fields['candidate'].value.title().split(",")
+	category = None if 'category' not in fields else fields['category'].value
 	dateFromStr = fields['dateFrom'].value
 	dateToStr = fields['dateTo'].value
 
 	dateFrom = datetime.strptime(dateFromStr, '%Y-%m-%d')
 	dateTo = datetime.strptime(dateToStr, '%Y-%m-%d') + timedelta(days=1)
 
-
 	col = MongoClient().test.twitter_search_candidates
 
-	tweets = list(col.find({'search_info.keywords': candidateName, 'created_at_local': {'$gte': dateFrom, '$lt': dateTo}}).batch_size(10000))
+        #-----------------------------------------------------------------------------
+        def doQuery(candidate):
+                query = {'search_info.keywords': candidate, 'created_at_local': {'$gte': dateFrom, '$lt': dateTo}}
+                if category:
+                        query['categories'] = category
+                
+                tweets = list(col.find(query).batch_size(10000))
 	 
-	entitySets = [t for t in tweets if 'entities' in t]
-
-	hashCounter = Counter([hashtag for t in entitySets if 'hashtags' in t['entities'] for hashtag in t['entities']['hashtags']])
-	urlCounter = Counter([url for t in entitySets if 'long_urls' in t['entities'] for url in t['entities']['long_urls']])
-	mentionCounter = Counter([mention for t in entitySets if 'user_mentions' in t['entities'] for mention in t['entities']['user_mentions']])
-	userCounter = Counter([t['user']['screen_name'] for t in tweets])
-
-	topUrls = urlCounter.most_common(MAX_RESULTS)
-	topHashes = hashCounter.most_common(MAX_RESULTS)
-	topMentions = mentionCounter.most_common(MAX_RESULTS)
-	topUsers = userCounter.most_common(MAX_RESULTS)
+                entitySets = [t for t in tweets if 'entities' in t]
 
 
-	rtCounter = Counter([t['retweeted_id'] for t in tweets if 'retweeted_id' in t])
-	topRTs = rtCounter.most_common(MAX_RESULTS)
-	topRTTexts = []
+                hashCounter = Counter([hashtag.lower() for t in entitySets if 'hashtags' in t['entities'] for hashtag in t['entities']['hashtags']])
+                urlCounter = Counter([url for t in entitySets if 'long_urls' in t['entities'] for url in t['entities']['long_urls']])
+                mentionCounter = Counter([mention for t in entitySets if 'user_mentions' in t['entities'] for mention in t['entities']['user_mentions']])
+                userCounter = Counter([t['user']['screen_name'] for t in tweets])
 
-	for indx, item in enumerate(topRTs):
-		retweeted = col.find_one({'_id': item[0]})
-		if retweeted and 'text' in retweeted:
-			topRTTexts.append({'value': retweeted['text'], 'count': item[1], 'url': TWEET_URL % (retweeted['user']['screen_name'], retweeted['id']), 'rank': indx + 1, 'profile_image':retweeted['user']['profile_image_url'] if 'profile_image_url' in retweeted['user'] else null, 'profile_screenName': retweeted['user']['screen_name'], 'profile_id': retweeted['user']['id']})
-		
+                topUrls = urlCounter.most_common(MAX_RESULTS)
+                topHashes = hashCounter.most_common(MAX_RESULTS)
+                topMentions = mentionCounter.most_common(MAX_RESULTS)
+                topUsers = userCounter.most_common(MAX_RESULTS)
 
-	output = {}
-	output['candidate'] = candidateName
-	output['urls'] = [{'value': item[0], 'url': item[0], 'count': item[1], 'rank': indx + 1} for indx, item in enumerate(topUrls)]
-	output['hashtags'] = [{'value': '#' + item[0], 'url': HASH_URL % item[0], 'count': item[1], 'rank': indx + 1} for indx, item in enumerate(topHashes)]
-	output['mentions'] = [{'value': '@' + item[0], 'url': USER_URL % item[0], 'count': item[1], 'rank': indx + 1} for indx, item in enumerate(topMentions)]
-	output['users'] = [{'value': item[0], 'url': USER_URL % item[0], 'count': item[1], 'rank': indx + 1} for indx, item in enumerate(topUsers)]
-	output['retweets'] = topRTTexts
 
-	import texty
-	text = ' '.join([t['text'] for t in tweets])
-	output['word_frequencies'] = [{'count': count, 'value': value} for value, count in texty.getWordFreqsAsOrderedList(text)[:50]]
+                rtCounter = Counter([t['retweeted_id'] for t in tweets if 'retweeted_id' in t])
+                topRTs = rtCounter.most_common(MAX_RESULTS)
+                topRTTexts = []
+
+                for indx, item in enumerate(topRTs):
+                        retweeted = col.find_one({'_id': item[0]})
+                        if retweeted and 'text' in retweeted:
+                                topRTTexts.append({'value': retweeted['text'], 'count': item[1], 'url': TWEET_URL % (retweeted['user']['screen_name'], retweeted['id']), 'rank': indx + 1, 'profile_image':retweeted['user']['profile_image_url'] if 'profile_image_url' in retweeted['user'] else null, 'profile_screenName': retweeted['user']['screen_name'], 'profile_id': retweeted['user']['id']})
+                        
+
+                output = {}
+                output['candidate'] = candidate
+                output['topWebpages'] = [{'value': item[0], 'url': item[0], 'count': item[1], 'rank': indx + 1} for indx, item in enumerate(topUrls)]
+                output['topHashtags'] = [{'value': '#' + item[0], 'url': HASH_URL % item[0], 'count': item[1], 'rank': indx + 1} for indx, item in enumerate(topHashes)]
+                output['topMentions'] = [{'value': '@' + item[0], 'url': USER_URL % item[0], 'count': item[1], 'rank': indx + 1} for indx, item in enumerate(topMentions)]
+                output['topChatters'] = [{'value': item[0], 'url': USER_URL % item[0], 'count': item[1], 'rank': indx + 1} for indx, item in enumerate(topUsers)]
+                output['topRetweets'] = topRTTexts
+
+                return output
+        #-----------------------------------------------------------------------------       
+	
+
+        #query each candidate
+        output={}
+        for candidate in candidateName:
+                output[candidate]=doQuery(candidate)
+	
+
+	#import texty
+	#text = ' '.join([t['text'] for t in tweets])
+	#output['word_frequencies'] = [{'count': count, 'value': value} for value, count in texty.getWordFreqsAsOrderedList(text)[:50]]
 	
 	print ''
 	print json.dumps(output)
---
+++ ws/getMetrics_noCategory.py
@@ -0,0 +1,69 @@
+#Standard Libraries
+import cgitb, os, cgi, json
+cgitb.enable()
+
+MAX_RESULTS = 10
+
+from pymongo import MongoClient
+from collections import Counter
+from datetime import datetime, timedelta
+
+USER_URL = 'http://www.twitter.com/%s'
+TWEET_URL = 'http://www.twitter.com/%s/status/%d'
+HASH_URL = 'https://twitter.com/search?q=%%23%s&src=hash'
+
+try: 
+	fields = cgi.FieldStorage()
+
+	candidateName = fields['candidate'].value.title()
+	dateFromStr = fields['dateFrom'].value
+	dateToStr = fields['dateTo'].value
+
+	dateFrom = datetime.strptime(dateFromStr, '%Y-%m-%d')
+	dateTo = datetime.strptime(dateToStr, '%Y-%m-%d') + timedelta(days=1)
+
+
+	col = MongoClient().test.twitter_search_candidates
+
+	tweets = list(col.find({'search_info.keywords': candidateName, 'created_at_local': {'$gte': dateFrom, '$lt': dateTo}}).batch_size(10000))
+	 
+	entitySets = [t for t in tweets if 'entities' in t]
+
+	hashCounter = Counter([hashtag for t in entitySets if 'hashtags' in t['entities'] for hashtag in t['entities']['hashtags']])
+	urlCounter = Counter([url for t in entitySets if 'long_urls' in t['entities'] for url in t['entities']['long_urls']])
+	mentionCounter = Counter([mention for t in entitySets if 'user_mentions' in t['entities'] for mention in t['entities']['user_mentions']])
+	userCounter = Counter([t['user']['screen_name'] for t in tweets])
+
+	topUrls = urlCounter.most_common(MAX_RESULTS)
+	topHashes = hashCounter.most_common(MAX_RESULTS)
+	topMentions = mentionCounter.most_common(MAX_RESULTS)
+	topUsers = userCounter.most_common(MAX_RESULTS)
+
+
+	rtCounter = Counter([t['retweeted_id'] for t in tweets if 'retweeted_id' in t])
+	topRTs = rtCounter.most_common(MAX_RESULTS)
+	topRTTexts = []
+
+	for indx, item in enumerate(topRTs):
+		retweeted = col.find_one({'_id': item[0]})
+		if retweeted and 'text' in retweeted:
+			topRTTexts.append({'value': retweeted['text'], 'count': item[1], 'url': TWEET_URL % (retweeted['user']['screen_name'], retweeted['id']), 'rank': indx + 1, 'profile_image':retweeted['user']['profile_image_url'] if 'profile_image_url' in retweeted['user'] else null, 'profile_screenName': retweeted['user']['screen_name'], 'profile_id': retweeted['user']['id']})
+		
+
+	output = {}
+	output['candidate'] = candidateName
+	output['urls'] = [{'value': item[0], 'url': item[0], 'count': item[1], 'rank': indx + 1} for indx, item in enumerate(topUrls)]
+	output['hashtags'] = [{'value': '#' + item[0], 'url': HASH_URL % item[0], 'count': item[1], 'rank': indx + 1} for indx, item in enumerate(topHashes)]
+	output['mentions'] = [{'value': '@' + item[0], 'url': USER_URL % item[0], 'count': item[1], 'rank': indx + 1} for indx, item in enumerate(topMentions)]
+	output['users'] = [{'value': item[0], 'url': USER_URL % item[0], 'count': item[1], 'rank': indx + 1} for indx, item in enumerate(topUsers)]
+	output['retweets'] = topRTTexts
+
+	import texty
+	text = ' '.join([t['text'] for t in tweets])
+	output['word_frequencies'] = [{'count': count, 'value': value} for value, count in texty.getWordFreqsAsOrderedList(text)[:50]]
+	
+	print ''
+	print json.dumps(output)
+except Exception, e:
+	print ''
+	print json.dumps(str(e))
